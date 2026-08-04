c43bb338126018ad1f7abe170b3a6df2a1f8c0e6
Mohamed Amr <MohxmedAmr@gmail.com>
2026-08-04 07:36:14 +0300

    Refactor Hero Section

--- src/features/landing/sections/HeroSection.js
+++ src/features/landing/sections/HeroSection.js
@@ -145,7 +145,7 @@ function HeroBackground() {
                 rotate: { duration: 6, repeat: Infinity, ease: "easeInOut" },
               }
         }
-        className="absolute right-[5%] top-[8%] hidden h-80 w-80 lg:block pointer-events-none z-0"
+        className="absolute left-[5%] top-[8%] hidden h-80 w-80 lg:block pointer-events-none z-0"
       >
         <Image
           src={roket}
